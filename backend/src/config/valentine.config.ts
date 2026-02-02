f40a830c12c202a2b33124e625efbc9115e54e96
Akash241-coder <akashkofficial0204@gmail.com>
2026-02-02 21:40:23 +0530

    final structure for github pages

--- backend/src/config/valentine.config.ts
+++
@@ -1,9 +0,0 @@
-export const valentineConfig = () => ({
-  name: process.env.VALENTINE_NAME,
-  message: process.env.VALENTINE_MESSAGE,
-  gifts: [
-    process.env.GIFT_LINK_1,
-    process.env.GIFT_LINK_2,
-    process.env.GIFT_LINK_3,
-  ],
-});
